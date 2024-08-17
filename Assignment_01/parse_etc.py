553354c81c022745fab02219c903d2c7f39f23c1
SudoGsxr <62088724+SudoGsxr@users.noreply.github.com>
2024-08-17 18:53:32 +0200

    assignment 01

---
+++ Assignment_01/parse_etc.py
@@ -0,0 +1,10 @@
+passwd = open("passwd", "r")
+users= {}
+
+for line in passwd:
+    splited_line = line.split(":")
+    username=splited_line[0]
+    user_data=[splited_line[5],splited_line[2]]
+    users.update({username:user_data})
+
+passwd.close()
