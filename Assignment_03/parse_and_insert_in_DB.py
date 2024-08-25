0ab95dfb2b3939716271f4d504bd58637e3c06ae
SudoGsxr <62088724+SudoGsxr@users.noreply.github.com>
2024-08-25 20:54:47 +0200

    update parse_and_insert_in_DB script

--- Assignment_03/parse_and_insert_in_DB.py
+++ Assignment_03/parse_and_insert_in_DB.py
@@ -1,17 +1,8 @@
+import click
 import mysql.connector
 
-mydb = mysql.connector.connect(
-  host="10.200.14.212",
-  user="ypython",
-  password="ypython",
-  database="Users"
-)
-
-mycursor = mydb.cursor()
-filename = "passwd"
-
-def parse_userdata(filename):
-    passwd = open(filename, "r")
+def parse_userdata(passwd_path):
+    passwd = open(passwd_path, "r")
     users_list= []
 
     for line in passwd:
@@ -20,14 +11,32 @@ def parse_userdata(filename):
         users_list.append(temp_vaules_)
     passwd.close()
     return users_list
-    
 
-def insert_into_DB(users_list):
-        for user in users_list:
-            sql = "INSERT INTO User_Details (id, Username, Home_Directory, Login_Shell) VALUES (%s, %s, %s, %s)"
-            mycursor.execute(sql,user)
-            mydb.commit()
-        print ("Data instered into DB sucessfully")
-        
+def insert_into_DB(users_list,db,u,p):
+    
+    mydb = mysql.connector.connect(
+        host=db,
+        user=u,
+        password=p,
+        database="Users"
+        )
+    mycursor = mydb.cursor()
+
+    for user in users_list:
+        sql = "INSERT INTO User_Details (id, Username, Home_Directory, Login_Shell) VALUES (%s, %s, %s, %s)"
+        mycursor.execute(sql,user)
+        mydb.commit()
+    print ("Data instered into DB sucessfully")
+
+@click.command()
+@click.option('--db', required=True ,help='Database IP Address or Hostname')
+@click.option('--u', required=True ,help='Database username')
+@click.option('--p', prompt=True, hide_input=True , help="Database password of the user" )
+@click.option('--passwd_path', prompt=True, required=True , help="path of the passwd file" )
+
+def cli(passwd_path,db,u,p):
+    insert_into_DB ( parse_userdata(passwd_path),db,u,p)
+
 if __name__ == "__main__":
-    insert_into_DB(parse_userdata(filename))
+    cli ()
+    
